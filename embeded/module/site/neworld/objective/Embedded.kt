7a66b79ed572b97c4bc6eb8a7e82a56eb04d06d1
DWVoid <yshliu0321@icloud.com>
2020-09-04 12:55:11 +0800

    maven publish code

--- embeded/module/site/neworld/objective/Embedded.kt
+++ embeded/module/site/neworld/objective/Embedded.kt
@@ -1,12 +1,32 @@
 package site.neworld.objective
 
+import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import site.neworld.objective.data.Concurrency
+import site.neworld.objective.data.LevelStorage
 import site.neworld.objective.data.consoleRecorder
+import java.io.File
 
-fun main() {
-    startKoin {
-        modules(consoleRecorder)
+object Controller {
+    private var app: KoinApplication? = null
+    private var counter = 0
+
+    private fun start() {
+        app = startKoin {
+            modules(consoleRecorder)
+        }
     }
-    Concurrency.join()
+
+    private fun stop() {
+        Concurrency.join()
+    }
+
+    fun open(path: File): LevelStorage {
+        return LevelStorage(path)
+    }
+
+    fun close(storage: LevelStorage) {
+        storage.close()
+        if (synchronized(this) { --counter } == 0) stop()
+    }
 }
